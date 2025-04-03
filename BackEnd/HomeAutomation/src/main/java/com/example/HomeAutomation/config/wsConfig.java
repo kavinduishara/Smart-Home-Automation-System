1b28d28b16552bf8f8fd1c9fbe6d6a14e7672d0d
kavindu ishara <kavinduishara2923@gmail.com>
2025-04-03 17:37:21 +0530

    Update wsConfig.java

--- BackEnd/HomeAutomation/src/main/java/com/example/HomeAutomation/config/wsConfig.java
+++ BackEnd/HomeAutomation/src/main/java/com/example/HomeAutomation/config/wsConfig.java
@@ -32,8 +32,7 @@ public class wsConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("ws")
-                .setAllowedOriginPatterns("*","http://127.0.0.1:5500/*")
-                .withSockJS();
+                .setAllowedOriginPatterns("*","http://127.0.0.1:5500/*");
     }
 
     @Override
